0bd9c04e0c098a3fc7b526d86d9c6ac55850af47
sachinsav <sachin.saw.13@gmai.com>
2020-12-27 23:01:34 +0530

    initial level read and delete operation addded

--- datastore.py
+++ datastore.py
@@ -1,5 +1,7 @@
 import pickle
 import os
+import json
+
 class datastore:
     def __init__(self,path="D:/datastore"):
         filepath = os.path.join(path, 'datastore')
@@ -29,11 +31,25 @@ class datastore:
         except:
             pass
 
-    def create(self,key="name2",val="sachin"):
-        self.data[key] = val
+    def create(self,key="name3",val={"a":5}):
+        if key in self.data:
+            raise Exception("given key is already exists")
+        else:
+            val = json.dumps(val)
+            self.data[key] = val
+
+    def read(self,key):
+        return self.data.get(key)
+    def delete(self,key):
+        if key not in self.data:
+            raise Exception("given key does not exists")
+        return self.data.pop(key)
         
 
 db = datastore()
 # db.create()
 # db.commit()
 print(db.getdata())
+print(db.read("name3"))
+db.delete("name3")
+# db.commit()
